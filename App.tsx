3c6fa43850a066cdd7d0318e5e04656905ae8abf
Roy Chan <royleochan@gmail.com>
2022-07-07 20:25:31 +0800

    Revert "feat: integrate amplify"
    This reverts commit 24cdbeeeecf76984a12aaf3dd59117f3f3377d91.

--- App.tsx
+++ App.tsx
@@ -1,7 +1,5 @@
-import { Amplify } from "aws-amplify";
-import awsconfig from "./src/aws-exports";
-
 import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+
 import useStore from "hooks/useStore";
 import LoginScreen from "screens/LoginScreen";
 import MainNavigator from "navigation/MainNavigator";
@@ -16,7 +14,6 @@ const theme = {
 };
 
 export default function App() {
-  Amplify.configure(awsconfig);
   const isAuthenticated = useStore((state) => state.isAuthenticated);
 
   return (

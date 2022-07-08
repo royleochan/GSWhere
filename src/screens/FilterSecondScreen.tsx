13b84f2060b8905b94659bdcef704130b5cfdf9b
e0712620 <e0712620@u.nus.edu>
2022-07-08 17:17:26 +0800

    Update FilterSec

--- src/screens/FilterSecondScreen.tsx
+++ src/screens/FilterSecondScreen.tsx
@@ -1,11 +1,217 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Dimensions, SafeAreaView, ScrollView } from "react-native";
+import SelectDropdown from 'react-native-select-dropdown'
+
+const {width} = Dimensions.get('window');
+
+const SeatType = ["Normal Desk", "Trading Desk"];
+
+var myDate = new Date();
+var dateArr = [];
+var dateTemp;
+for (var i = 0; i < 7; i++) {
+  var month = myDate.getMonth() + 1;
+  var date = myDate.getDate();
+  var year = myDate.getFullYear();
+  month < 10 ? (month = "0" + month) : month;
+  date < 10 ? (date = "0" + date) : date;
+  dateTemp = year + "-" + month + "-" + date;
+  dateArr.push(dateTemp);
+  myDate.setDate(myDate.getDate() + 1);
+  }
+console.log(dateArr);
 
 const FilterSecondScreen = () => {
   return (
-    <View>
-      <Text>FilterSecondScreen</Text>
+    <View style={styles.viewContainer}>
+      <ScrollView
+          showsVerticalScrollIndicator={false}
+          alwaysBounceVertical={false}
+          contentContainerStyle={styles.scrollViewContainer}>
+
+        <Text style={styles.txt}>Date</Text>
+        <SelectDropdown 
+          data={dateArr}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index)
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem
+          }}
+          rowTextForSelection={(item, index) => {
+            return item
+          }}
+          buttonStyle={styles.dropdown1BtnStyle}
+          buttonTextStyle={styles.dropdown1BtnTxtStyle}
+          dropdownIconPosition={'right'}
+          dropdownStyle={styles.dropdown1DropdownStyle}
+          rowStyle={styles.dropdown1RowStyle}
+          rowTextStyle={styles.dropdown1RowTxtStyle}
+        />
+
+        <Text style={styles.txt}>Desk Type</Text>
+        <SelectDropdown
+          data={SeatType}
+          onSelect={(selectedItem, index) => {
+            console.log(selectedItem, index)
+          }}
+          buttonTextAfterSelection={(selectedItem, index) => {
+            return selectedItem
+          }}
+          rowTextForSelection={(item, index) => {
+            return item
+          }}
+          buttonStyle={styles.dropdown1BtnStyle}
+          buttonTextStyle={styles.dropdown1BtnTxtStyle}
+          dropdownIconPosition={'right'}
+          dropdownStyle={styles.dropdown1DropdownStyle}
+          rowStyle={styles.dropdown1RowStyle}
+          rowTextStyle={styles.dropdown1RowTxtStyle}
+        />
+
+        
+      </ScrollView>
     </View>
+
   );
 };
+
+const styles = StyleSheet.create({
+  txt: {
+    fontSize: 20,
+    color: "#7D9CC0",
+    paddingLeft: 0,
+    paddingTop: 30,
+    paddingBottom: 15,
+    // justifyContent: "space-between"
+  },
+  hdr: {
+    marginBottom: "30%"
+  },
+  box: {
+    display: "flex",
+    justifyContent: "center",
+    width: "80%"
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 6},
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    elevation: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    width,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F6F6F6',
+  },
+  headerTitle: {color: '#000', fontWeight: 'bold', fontSize: 16},
+  saveAreaViewContainer: {flex: 1, backgroundColor: '#FFF'},
+  viewContainer: {marginBottom:"30%", width},
+  scrollViewContainer: {
+    flexGrow: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: '10%',
+    paddingBottom: '20%',
+  },
+
+  dropdown1BtnStyle: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#444',
+  },
+  dropdown1BtnTxtStyle: {color: '#444', textAlign: 'left'},
+  dropdown1DropdownStyle: {backgroundColor: '#EFEFEF'},
+  dropdown1RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
+  dropdown1RowTxtStyle: {color: '#444', textAlign: 'left'},
+
+  dropdown2BtnStyle: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#444',
+    borderRadius: 8,
+  },
+  dropdown2BtnTxtStyle: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  dropdown2DropdownStyle: {
+    backgroundColor: '#444',
+    borderBottomLeftRadius: 12,
+    borderBottomRightRadius: 12,
+  },
+  dropdown2RowStyle: {backgroundColor: '#444', borderBottomColor: '#C5C5C5'},
+  dropdown2RowTxtStyle: {
+    color: '#FFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+  dropdown3BtnStyle: {
+    width: '80%',
+    height: 50,
+    backgroundColor: '#FFF',
+    paddingHorizontal: 0,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: '#444',
+  },
+  dropdown3BtnChildStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+  },
+  dropdown3BtnImage: {width: 45, height: 45, resizeMode: 'cover'},
+  dropdown3BtnTxt: {
+    color: '#444',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginHorizontal: 12,
+  },
+  dropdown3DropdownStyle: {backgroundColor: 'slategray'},
+  dropdown3RowStyle: {
+    backgroundColor: 'slategray',
+    borderBottomColor: '#444',
+    height: 50,
+  },
+  dropdown3RowChildStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingHorizontal: 18,
+  },
+  dropdownRowImage: {width: 45, height: 45, resizeMode: 'cover'},
+  dropdown3RowTxt: {
+    color: '#F1F1F1',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginHorizontal: 12,
+  },
+
+  dropdown4BtnStyle: {
+    width: '50%',
+    height: 50,
+    backgroundColor: '#FFF',
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#444',
+  },
+  dropdown4BtnTxtStyle: {color: '#444', textAlign: 'left'},
+  dropdown4DropdownStyle: {backgroundColor: '#EFEFEF'},
+  dropdown4RowStyle: {backgroundColor: '#EFEFEF', borderBottomColor: '#C5C5C5'},
+  dropdown4RowTxtStyle: {color: '#444', textAlign: 'left'},
+});
 
 export default FilterSecondScreen;

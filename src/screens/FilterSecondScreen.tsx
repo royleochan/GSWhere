336e201dafb5117461d922922c5256052edf3069
munharsha <munharsha@gmail.com>
2022-07-08 16:41:27 +0800

    fix merge

--- src/screens/FilterSecondScreen.tsx
+++ src/screens/FilterSecondScreen.tsx
@@ -2,8 +2,6 @@ import SelectDropdown from "react-native-select-dropdown";
 import * as React from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Button } from "react-native-paper";
-
-<<<<<<< HEAD
 import MainButton from "components/MainButton";
 import useChangeOrientation from "hooks/useChangeOrientation";
 
@@ -18,10 +16,6 @@ const FilterSecondScreen = ({ navigation }) => {
       </MainButton>
     </View>
   );
-=======
-const FilterSecondScreen = () => {
-  return <View></View>;
->>>>>>> Filter_page
 };
 
 export default FilterSecondScreen;

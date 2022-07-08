c39bef31aa547bba8d502e9532ae52299bdd5780
Roy Chan <royleochan@gmail.com>
2022-07-08 18:00:24 +0800

    feat: integrate FE and BE

--- src/components/Seat.tsx
+++ src/components/Seat.tsx
@@ -1,15 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { Badge } from "react-native-paper";
 
-const Seat = () => {
+const Seat = ({ name, isReserved, onPress }) => {
   return (
     <View
       style={{
-        width: 20,
+        width: 40,
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
-      <Badge size={30} style={{ backgroundColor: "blue" }}></Badge>
-      <Text>Id</Text>
+      <TouchableOpacity onPress={onPress}>
+        <Badge
+          size={30}
+          style={{ backgroundColor: isReserved ? "red" : "blue" }}
+        ></Badge>
+      </TouchableOpacity>
+      <Text style={{ width: 50 }}>{name}</Text>
     </View>
   );
 };
--- src/screens/FloorPlanScreen.tsx
+++ src/screens/FloorPlanScreen.tsx
@@ -1,40 +1,89 @@
 import { View, ScrollView, StyleSheet, Text } from "react-native";
+import { Modal, Portal, Provider, Button } from "react-native-paper";
 import { Image } from "react-native";
 
 import Seat from "components/Seat";
 import useChangeOrientation from "hooks/useChangeOrientation";
 import chunk from "utils/chunk.js";
+import { useState, useEffect } from "react";
+import request from "utils/request";
 
-const FloorPlanScreen = () => {
+const FloorPlanScreen = ({ navigation }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedDesk, setSelectedDesk] = useState(null);
+  const [desks, setDesks] = useState([]);
   useChangeOrientation("landscape");
-  const arr = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ];
+  const containerStyle = { backgroundColor: "white", padding: 20 };
+
+  const handleClickSeat = (desk) => {
+    setIsModalOpen(true);
+    setSelectedDesk(desk);
+  };
+
+  const navigateToProfile = () => {
+    navigation.navigate("My Bookings");
+  };
+
+  useEffect(() => {
+    const getDesks = async () => {
+      const result = await request.get("/desks");
+      const desks = await result.data.Items;
+      setDesks(desks);
+    };
+    getDesks();
+  }, []);
 
   return (
     <ScrollView style={styles.container}>
       <Text style={{ fontSize: 26 }}>Maple Tree Anson - L16</Text>
-      <View style={{ marginTop: 10 }}>
-        <Image source={require("../../assets/office.png")} />
-      </View>
-      <View style={{ margin: 24 }}>
-        {chunk(arr, 10).map((row, i) => {
-          return (
-            <View
-              key={i}
-              style={{
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-                marginVertical: 5,
-              }}
-            >
-              {row.map((_, i) => (
-                <Seat key={i} />
-              ))}
-            </View>
-          );
-        })}
-      </View>
+      <Provider>
+        <Portal>
+          <Modal
+            visible={isModalOpen}
+            onDismiss={() => setIsModalOpen(false)}
+            contentContainerStyle={containerStyle}
+          >
+            {selectedDesk !== null && (
+              <View>
+                <Text>{`Desk id: ${selectedDesk.id}`}</Text>
+                <Text>{`Desk type: ${selectedDesk["employee_type"]}`}</Text>
+                <Text>{`Kerberos: ${selectedDesk.kerberos}`}</Text>
+                <Text>{`Monitor Specs: ${selectedDesk["monitor_specs"]}`}</Text>
+                <Text>{`Phone Type: ${selectedDesk["phone_type"]}`}</Text>
+                {!selectedDesk["reservation_status"] && (
+                  <Button onPress={navigateToProfile}>Confirm Booking</Button>
+                )}
+              </View>
+            )}
+          </Modal>
+        </Portal>
+        <View style={{ marginTop: 10 }}>
+          <Image source={require("../../assets/office.png")} />
+        </View>
+        <View style={{ margin: 24 }}>
+          {chunk(desks, 5).map((row, i) => {
+            return (
+              <View
+                key={i}
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "space-evenly",
+                  marginVertical: 5,
+                }}
+              >
+                {row.map((desk) => (
+                  <Seat
+                    key={desk.id}
+                    name={desk.id}
+                    isReserved={desk["reservation_status"]}
+                    onPress={() => handleClickSeat(desk)}
+                  />
+                ))}
+              </View>
+            );
+          })}
+        </View>
+      </Provider>
     </ScrollView>
   );
 };

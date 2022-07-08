7a28949849e7367ebcbdb449f6f8c49a7b255c63
Roy Chan <royleochan@gmail.com>
2022-07-08 14:20:46 +0800

    feat: add request util

---
+++ src/utils/request.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://tvqa8lc1dl.execute-api.us-east-1.amazonaws.com",
+  timeout: 20000,
+});
+
+const request = {
+  get: (url, settings) => {
+    return api.get(url, settings);
+  },
+  post: (url, data, token) => {
+    if (token) {
+      return api.post(url, data, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+    }
+    return api.post(url, data);
+  },
+  patch: (url, data, token) => {
+    return api.patch(url, data, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+  },
+  delete: (url, token) => {
+    return api.delete(url, {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+  },
+};
+
+export default request;
